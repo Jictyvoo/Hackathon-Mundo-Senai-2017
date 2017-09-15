84794ddb7a07270fe58e44d029b5785e488d4cc2
franciscotis <franncisco.p@gmail.com>
2017-09-15 06:58:59 -0300

    mine

--- web_system/views/mobileGame.php
+++ web_system/views/mobileGame.php
@@ -1,4 +1,4 @@
 <?php
 ?>
-
-<a href="../internal_files/Automedicacao_Games.jar" download>Clique aqui para fazer o download</a>
+<p align="center" ><img src="imagem/Untitled1.gif"></p> <br>
+<p align="center"><a href="../internal_files/Automedicacao_Games.jar" download>Clique aqui para fazer o download</a></p>
