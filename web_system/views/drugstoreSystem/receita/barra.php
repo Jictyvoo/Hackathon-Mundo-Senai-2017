cf955fb40ae7dfeae0b84f29857521038c189512
Jictyvoo <jictyvoo.ecomp@gmail.com>
2017-09-15 07:03:41 -0300

    vjhfvel.wmgdrjged

--- web_system/views/drugstoreSystem/receita/barra.php
+++ web_system/views/drugstoreSystem/receita/barra.php
@@ -1,6 +1,81 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-    "http://www.w3.org/TR/html4/strict.dtd"
-    >
+<?php
+	function geraCodigoBarra($numero){
+		$fino = 1;
+		$largo = 3;
+		$altura = 50;
+		
+		$barcodes[0] = '00110';
+		$barcodes[1] = '10001';
+		$barcodes[2] = '01001';
+		$barcodes[3] = '11000';
+		$barcodes[4] = '00101';
+		$barcodes[5] = '10100';
+		$barcodes[6] = '01100';
+		$barcodes[7] = '00011';
+		$barcodes[8] = '10010';
+		$barcodes[9] = '01010';
+		
+		for($f1 = 9; $f1 >= 0; $f1--){
+			for($f2 = 9; $f2 >= 0; $f2--){
+				$f = ($f1*10)+$f2;
+				$texto = '';
+				for($i = 1; $i < 6; $i++){
+					$texto .= substr($barcodes[$f1], ($i-1), 1).substr($barcodes[$f2] ,($i-1), 1);
+				}
+				$barcodes[$f] = $texto;
+			}
+		}
+		
+		echo '<img src="imagens/p.gif" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		echo '<img src="imagens/b.gif" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		echo '<img src="imagens/p.gif" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		echo '<img src="imagens/b.gif" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		
+		echo '<img ';
+		
+		$texto = $numero;
+		
+		if((strlen($texto) % 2) <> 0){
+			$texto = '0'.$texto;
+		}
+		
+		while(strlen($texto) > 0){
+			$i = round(substr($texto, 0, 2));
+			$texto = substr($texto, strlen($texto)-(strlen($texto)-2), (strlen($texto)-2));
+			
+			if(isset($barcodes[$i])){
+				$f = $barcodes[$i];
+			}
+			
+			for($i = 1; $i < 11; $i+=2){
+				if(substr($f, ($i-1), 1) == '0'){
+  					$f1 = $fino ;
+  				}else{
+  					$f1 = $largo ;
+  				}
+  				
+  				echo 'src="imagens/p.gif" width="'.$f1.'" height="'.$altura.'" border="0">';
+  				echo '<img ';
+  				
+  				if(substr($f, $i, 1) == '0'){
+					$f2 = $fino ;
+				}else{
+					$f2 = $largo ;
+				}
+				
+				echo 'src="imagens/b.gif" width="'.$f2.'" height="'.$altura.'" border="0">';
+				echo '<img ';
+			}
+		}
+
+			echo 'src="imagens/p.gif" width="'.$largo.'" height="'.$altura.'" border="0" />';
+			echo '<img src="imagens/b.gif" width="'.$fino.'" height="'.$altura.'" border="0" />';
+			echo '<img src="imagens/p.gif" width="1" height="'.$altura.'" border="0" />';
+	}
+
+	geraCodigoBarra('0123456789');
+?>
+
 <html lang="en">
 <head>
     <title>Receita médica</title>
@@ -116,7 +191,7 @@
 </style>
 <body>
     <div id="wrapper">
-        <form action="barra.php" method="post">
+        <form action="" method="post">
             <fieldset>
                 <legend>Receita</legend>
                 <div>
@@ -135,9 +210,10 @@
                     <div class="small">Prescrição de medicamentos e tratamentos específicos</div>
                     <textarea name="prescricao" placeholder="Prescrição"></textarea>
                 </div>    
-                <input type="submit" name="submit" value="Gerar receita"/>
             </fieldset>    
         </form>
     </div>
 </body>
 </html>
+
+
